50b42d1e99c73fad0ac927e361557724f7e99a30
Andrew Mason <andrewmcodes@protonmail.com>
2020-04-21 22:52:37 -0400

    update application.rb and scaffold posts

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module TailwindCss134RailsDemo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.stylesheets false
+    end
   end
 end
